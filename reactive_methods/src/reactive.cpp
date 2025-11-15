113dc2466207cf8bdb5217f9cc41764346a8205b
MorenaPrudo <doggosup@gmail.com>
2025-11-15 00:05:59 -0500

    debugging, changed find_best_point method

--- reactive_methods/src/reactive.cpp
+++ reactive_methods/src/reactive.cpp
@@ -1,8 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include <string>
 #include <vector>
+#include <iostream>
 #include "sensor_msgs/msg/laser_scan.hpp"
-#include "nav_msgs/msg/odometry.hpp"
 #include "ackermann_msgs/msg/ackermann_drive_stamped.hpp"
 #include <opencv2/opencv.hpp>
 #include <utility>
@@ -15,7 +15,7 @@ public:
     {
         drive_publisher = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>(drive_topic,10);
         laser_subscriber = this->create_subscription<sensor_msgs::msg::LaserScan>(lidarscan_topic,10,
-        [this](sensor_msgs::msg::LaserScan::ConstSharedPtr msg){lidar_callback(msg)});
+        [this](sensor_msgs::msg::LaserScan::ConstSharedPtr msg){lidar_callback(msg);});
     
     }
 
@@ -24,23 +24,26 @@ private:
     std::string drive_topic = "/drive";
     rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr drive_publisher;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_subscriber;
-    float VISION_ANGLE_MIN = -1.5707;
-    float VISION_ANGLE_MAX = 1.5707;
-    int CONV_SIZE = 3;
-    int MAX_LIDAR_DIST = 3;  // in m
+    float VISION_ANGLE_MIN = -2.2;
+    float VISION_ANGLE_MAX = 2.2;
+    int CONV_SIZE = 5;
+    int BEST_POINT_CONV_SIZE = 75;
+    int MAX_LIDAR_DIST = 6;  // in m
     float BUBBLE_RADIUS = 0.5; // in m
-    float SPEED = 6;
+    float SPEED = 3;
+    float CORNER_ANGLE = 0.175;
+    float CORNER_SPEED = 1;
 
-    cv::Mat preprocess_lidar(float* ranges, float rad_increment, int length, float rad_min, float rad_max)
+    cv::Mat preprocess_lidar(std::vector<float> ranges, float rad_increment, int length, float rad_min, float rad_max)
     {   
         //coverting to a cv::Mat will make calculations easier
-        cv::Mat ranges_mat(1, length, CV_32F, ranges);
+        cv::Mat ranges_mat = cv::Mat(ranges).t();
 
         //slicing the lidar ranges to a specific range to only use what in front of the vehicle
-        int min_index = std::round(VISION_ANGLE_MIN - rad_min / rad_increment);
-        int max_index = std::round(VISION_ANGLE_MAX - rad_max / rad_increment);
-        cv::Mat sliced_ranges_mat = ranges_mat.colRange(min_index, length + max_index);
-
+        int min_index = std::round((VISION_ANGLE_MIN - rad_min) / rad_increment);
+        int max_index = std::round((VISION_ANGLE_MAX - rad_max) / rad_increment);
+        cv::Mat sliced_ranges_mat = ranges_mat.colRange(std::max(0,min_index), std::min(ranges_mat.cols, length + max_index));
+       
         //reducing noise in lidar data by setting each value to a mean
         cv::Mat kernel = cv::Mat::ones(1,CONV_SIZE, CV_32F);
         cv::Mat convolution;
@@ -74,18 +77,28 @@ private:
         
         if (current_gap > max_gap) {
             max_gap = current_gap;
-            end_i = i;
+            end_i = ranges.cols - 1;
         }
         
         //modifies the passed in indices array to the start and end indices of the max gap
         indices[0] = end_i - max_gap;
         indices[1] =  end_i;
+
     }
 
-    int find_best_point(int* indices)
+    int find_best_point(int* indices, cv::Mat ranges)
     {   
-        //returns index center of max gap
-        return (indices[0] + indices[1])/2;
+        cv::Mat gap = ranges.colRange(indices[0], indices[1]);
+        
+        cv::Mat kernel = cv::Mat::ones(1,BEST_POINT_CONV_SIZE, CV_32F);
+        cv::Mat convolution;
+        cv::filter2D(gap, convolution, -1, kernel);
+        convolution /= BEST_POINT_CONV_SIZE;
+        
+        cv::Point max_location;
+        cv::minMaxLoc(convolution, nullptr, nullptr, nullptr, &max_location);
+        int max_i = max_location.x + indices[0];
+        return max_i;
     }
 
     float get_angle(int index, int ranges_length, float angle_increment){
@@ -104,13 +117,14 @@ private:
         float angle_increment = scan_msg->angle_increment;
         float angle_min = scan_msg->angle_min;
         float angle_max = scan_msg->angle_max;
-        float* ranges = scan_msg->ranges;
-        int length = std::round((angle_max - angle_min)/angle_increment) + 1;
+        std::vector<float> ranges = scan_msg->ranges;
+        int length = ranges.size();
 
         cv::Mat processed_ranges = preprocess_lidar(ranges,angle_increment,length,angle_min,angle_max);
+        length = processed_ranges.cols;
 
         //create a safety bubble around the closest obstacle
-        float min;
+        double min;
         cv::Point min_location;
         cv::minMaxLoc(processed_ranges, &min, nullptr, &min_location, nullptr);
         int min_i = min_location.x;
@@ -118,32 +132,35 @@ private:
         //get angular size of bubble 
         //note the factor of 2 was omitted as it would be added to
         //both sides of closest obtacle location
-        float angular_size = std::atan(BUBBLE_RADIUS/min);
+        float angular_size = std::atan(BUBBLE_RADIUS/(float)min);
         
         //get angular size as indices
-        int indices = std::cell(angular_size/angle_increment);
+        int indices = std::ceil(angular_size/angle_increment);
 
         //make sure bubble indices wont be out of bounds
         int bubble_min_i = std::max(0,min_i - indices);
-        int bubble_max_i = std::min(length-1, indices + min_i)
+        int bubble_max_i = std::min(length-1, indices + min_i);
 
         //zero out all values in safety bubble
         processed_ranges.colRange(bubble_min_i, bubble_max_i) = 0;
 
         //get indices of the max gap (largest gap of freespace around the closest obstacle)
-        int* gap_indices;
+        int gap_indices[] = {0,0};
         find_max_gap(processed_ranges,gap_indices);
 
         //get steering angle from best point in max gap
-        float steering_angle = get_angle(find_best_point(gap_indices),length,angle_increment);
+        float steering_angle = get_angle(find_best_point(gap_indices,processed_ranges),length,angle_increment);
 
         //publish an Ackermann Drive Stamped message
-        auto msg = ackermann_msgs::AckermannDriveStamped();
+        auto msg = ackermann_msgs::msg::AckermannDriveStamped();
         msg.header.stamp = this->get_clock()->now();
         msg.header.frame_id = "base_link";
 
         msg.drive.speed = SPEED;
         msg.drive.steering_angle = steering_angle;
+        if (std::abs(steering_angle) > CORNER_ANGLE){
+           msg.drive.speed = CORNER_SPEED; 
+        }
 
         drive_publisher->publish(msg);
 
